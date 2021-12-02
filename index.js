9b7c86fd917328921625a19f07c11e021e58bb17
Rima Bouchakri <17460332+Rima-B@users.noreply.github.com>
2021-12-02 16:21:33 -0500

    minor fix

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Fetcher extends Airtable {
       ["ImagingSite", {}],
       ["ProcedureName", {}],
       ["Priority", {}],
-      ["ServiceType", {},
+      ["ServiceType", {}],
       ["ProcCancelReason", {}]
     ]
   }
